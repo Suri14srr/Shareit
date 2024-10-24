f1183507be796160e3a0be3fc87b0e34e6a1b30c
Krishna Kumar <krishnak@kognosdata.com>
2024-10-24 12:57:40 +0530

    added code for custome cmd

--- anyshare/models.py
+++ anyshare/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class UniqueURL(models.Model):
+    unique_id = models.UUIDField(default=uuid.uuid4, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def is_expired(self):
+        return timezone.now() > self.created_at + timedelta(minutes=1)
--- anyshare/views.py
+++ anyshare/views.py
@@ -1,12 +1,13 @@
-import uuid
-from django.shortcuts import redirect, render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import UniqueURL
 
 def base_redirect(request):
-    unique_id = uuid.uuid4()
-    return redirect('unique_view', unique_id=unique_id)
+    unique_url = UniqueURL.objects.create()
+    return redirect('unique_view', unique_id=unique_url.unique_id)
 
 def unique_view(request, unique_id):
-    # Render the template from templates/unique_page.html
+    unique_url = get_object_or_404(UniqueURL, unique_id=unique_id)
+    base_url = f'{request.scheme}://{request.get_host()}/'
+    if unique_url.is_expired:
+        return render(request, 'expired.html', {'base_url': base_url})  
     return render(request, 'unique_page.html', {'unique_id': unique_id})
